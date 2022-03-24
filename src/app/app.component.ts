ac71159311487a147addff87b84cc56b8e6c84b2
RushabhSOLACE <bagderushabh@gmail.com>
2022-03-24 11:13:31 +0530

    demo-code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
-
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-drag-drop';
+  // Transfer Items Between Lists
+  MoviesList = [
+    'The Far Side of the World',
+    'Morituri',
+    'Napoleon Dynamite',
+    'Pulp Fiction',
+    'Blade Runner',
+    'Cool Hand Luke',
+    'Heat',
+    'Juice'
+  ];
+  content:any=["Problem Statement","Solution","Planning","Prototyping",
+ "User Acceptance Test","Feedbacks","Agile","Frontend",
+ "Backend","Database","Hosting Infra","Cloud Computing ",
+ "Third Party Services","DevOps","Logical","Reusability",
+ "Scalability","Communication","Documentation","Quality Analysis",
+ "Unit Testing","Data Analyst","Digital Marketing","SEO",
+ "Back Office Mgmt","UI","UX","Code versioning",
+]
+  MoviesWatched:any = [
+  ];
+  onDrop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex);
+    }
+  }  
 }
